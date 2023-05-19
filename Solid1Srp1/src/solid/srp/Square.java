5ed0cf98476033d6c07e65becae57a70b7ec1f20
Vladimir Morozov <mrvlmor@gmail.com>
2023-05-19 19:52:45 +0300

    Update Square.java

--- Solid1Srp1/src/solid/srp/Square.java
+++ Solid1Srp1/src/solid/srp/Square.java
@@ -32,18 +32,18 @@ public class Square {
 
     public void draw() {
         for (int i = 0; i < side; i++) {
-            System.out.print("*");
+            System.out.print("* ");
         }
         System.out.println();
-        for (int i = 0; i < side; i++) {
-            System.out.print("*");
+        for (int i = 0; i < side-2; i++) {
+            System.out.print("* ");
             for (int j = 1; j < side - 1; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             System.out.println("*");
         }
         for (int i = 0; i < side; i++) {
-            System.out.print("*");
+            System.out.print("* ");
         }
     }
 }
